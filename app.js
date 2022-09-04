e999cb6d985c513cb8f2494904e04184721d4ca0
faresahmed1153 <faresahmed3090@gmail.com>
2022-09-04 13:14:44 +0200

    final

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 //jshint esversion:6
 require("dotenv").config();
 const express = require("express");
-const bodyParser = require("body-parser");
+
 const mongoose = require("mongoose");
 const _ = require("lodash");
 
 const app = express();
 
 app.set("view engine", "ejs");
-
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.dbLink);
